461b95fa35f8efa77c62d2ff9f0cd1a62c8394bb
trollbotlmfao <107155414+trollbotlmfao@users.noreply.github.com>
2025-03-16 17:50:28 -0400

    1

---
+++ src/App.js
@@ -0,0 +1,77 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+    <   h1>Random</h1>
+      </div>
+      <div class="profilepic">
+    <img src="https://fastly.picsum.photos/id/152/200/300.jpg?hmac=eCdUqkEQWPiigXtrPPzcwO9QeKYgOrV_YWW0LoFkuyk" alt="pfp" width="100px" height="100px" class="img"/>
+
+    <pre>
+      231
+      followers
+    </pre>
+    <pre>
+      152
+      following
+    </pre>
+    <pre>
+      20
+      posts
+    </pre>
+  </div>
+
+  <div class="info">
+    <pre>CSSG
+Hello, I'm CSSG!
+    </pre>
+
+  </div>
+
+  <div class="buttons">
+    <div class="rectangle">
+      Follow
+
+    </div>
+    <div class="rectangle1">
+      <p>Message</p>
+
+    </div>
+  </div>
+
+
+
+  <div class="symbols">
+    <img src="grid.jpg" alt=""/>
+    <img src="tagged.png" alt=""/>
+    <img src="reels.png" alt=""/>
+    
+  </div>
+
+
+  <div class="images">
+
+
+    <img src="https://fastly.picsum.photos/id/131/200/300.jpg?hmac=9q7mRSOguNBFGg_gPPRKlfjNINGjXWeDBTYPP1_gEas" alt="" class="posts"/>
+    <img src="https://fastly.picsum.photos/id/787/200/300.jpg?hmac=XItcL1ki66gQzP2FwRZjLbruiohUmaOYs9mmlDZe9KE" alt="" class="posts"/>
+    <img src="https://fastly.picsum.photos/id/287/200/300.jpg?hmac=9JSSeZMseJ8l_WTFAMmF3HXoyYmKFzQxmagpyWBefTA" alt="" class="posts"/>
+    <img src="https://fastly.picsum.photos/id/830/200/300.jpg?hmac=YHS3854_x-GHeQToxsiUmEvBJpDbZOAyixX9nxz61Sg" alt="" class="posts"/>
+    <img src="https://fastly.picsum.photos/id/94/200/300.jpg?hmac=CA7x5S3EdSeRqM9TK0RxiKTcx1R96lIncvKTjTP3beI" alt="" class="posts"/>
+    <img src="https://fastly.picsum.photos/id/958/200/300.jpg?hmac=oCwv3AFzS5VqZv3nvDJ3H5RzcDH2OiL2g-GGwWL5fsI" alt="" class="posts"/>
+    <img src="https://fastly.picsum.photos/id/379/200/300.jpg?hmac=IEcRQyv-DIaRsldE8jIdMRW5IHCTyemefU-bbCJpY34" alt="" class="posts"/>
+    <img src="https://fastly.picsum.photos/id/379/200/300.jpg?hmac=IEcRQyv-DIaRsldE8jIdMRW5IHCTyemefU-bbCJpY34" alt="" class="posts"/>
+    <img src="https://fastly.picsum.photos/id/572/200/300.jpg?hmac=Rt4zD8IxoA-nMVDrBQ72mgbTVRfQ6OwW3MhWy_3lpdk" alt="" class="posts"/>
+  </div>
+
+
+
+  </div>
+
+    
+  );
+}
+
+export default App;
